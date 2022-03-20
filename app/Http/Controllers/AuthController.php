2d12bf9557a427576dc744cd839ae9bb4f9ef8f3
Arvin Kent Lazaga <arvinkent121816@gmail.com>
2022-03-20 18:39:12 +0800

    Added Login and Logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
 
 use App\Http\Requests\RegisterRequest;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -16,6 +16,16 @@ class AuthController extends Controller
             'email' => $request->get('email'),
             'password' => bcrypt($request->get('password')),
         ]);
+    }
+
+    public function login(LoginRequest $request) {
+        $user = User::where('email', $request->get('email'))->first();
+
+        if (!$user || !Hash::check($request->get('password'), $user->password)) {
+            return response([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
 
         $token = $user->createToken('token-sample')->plainTextToken;
 
@@ -25,5 +35,13 @@ class AuthController extends Controller
         ];
 
         return response($response, 201);
+    }
+
+    public function logout() {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged out',
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -24,10 +24,14 @@ Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
 Route::post('/register', [AuthController::class, 'register']);
 
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/products', [ProductController::class, 'store']);
 
     Route::put('/products/{id}', [ProductController::class, 'update']);
 
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
